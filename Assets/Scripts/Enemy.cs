4be14f978b66bb3b7251f65fe50d52fe0a50ef4e
Davee Nguyen <vibrantdavee@gmail.com>
2013-06-03 16:46:04 -0700

    Rotation success

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,12 +16,13 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		_transform.position += Vector3.down*(speed*Time.deltaTime);
-		_transform.rotation = Quaternion.Lerp (_transform.rotation, Quaternion.LookRotation(Target.position - _transform.position), 100f*Time.deltaTime );
+		
+		//_transform.rotation = Quaternion.Lerp (_transform.rotation, Quaternion.LookRotation(Target.position - _transform.position), 100f*Time.deltaTime );
 	}
 	
 	void OnTriggerEnter( Collider collider ){
 		if( collider.gameObject.tag == "Player" ){
-			Instantiate( bullet, _transform.position, Quaternion.identity ); 
+			Instantiate( bullet, _transform.position, Quaternion.LookRotation(Vector3.forward, collider.transform.position - _transform.position) ); 
 			Destroy( gameObject );
 		}
 	}
