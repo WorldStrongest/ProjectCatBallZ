9af85176cad655131a44b96d3cc7c8f37f6100fe
Davee Nguyen <vibrantdavee@gmail.com>
2013-06-09 19:15:10 -0700

    Created Scenes folder and MasterSpawner

---
+++ Assets/Scripts/MasterSpawner.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+// Enabling Linq
+using System.Collections.Generic; //Always a good idea
+using System.Linq;
+
+[ExecuteInEditMode] // enables execution of script in edit mode
+public class MasterSpawner : MonoBehaviour {
+	
+	public SpawnerTimePair[] spawnTable;
+	public int spawnedCount;
+	
+	public float curTime;
+	
+	private int _tableSize;
+	private Transform _transform;
+	
+	// Use this for initialization
+	void Start () {
+		_transform = transform;
+		_tableSize = spawnTable.Length;
+		
+		spawnTable = spawnTable.OrderBy(st => st.spawnTime).ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.isPlaying)
+		{
+			curTime = Time.time;
+			
+			if ((spawnedCount < _tableSize) && (Time.time >= spawnTable[spawnedCount].spawnTime))
+			{
+				Instantiate(spawnTable[spawnedCount].spawner, _transform.position, _transform.rotation);
+				spawnedCount++;
+			} else if (spawnedCount >= _tableSize) {
+				Destroy(gameObject);
+			}
+		} else {
+			// else you're in edit mode. sort table by time
+			spawnTable = spawnTable.OrderBy(st => st.spawnTime).ToArray();
+		}
+	}
+	
+	[System.Serializable]
+	public class SpawnerTimePair {
+		public GameObject spawner;
+		public float spawnTime;
+	}
+}
+
