257499703335af2d8a9c89d1440c3e303affad3f
thomasndesign <thomasndesign@gmail.com>
2013-06-23 17:43:26 -0700

    Tom Test

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -6,6 +6,7 @@ public class Background : MonoBehaviour {
 	public Transform[] _bgs;
 	public int speed;
 	float amtToMove;
+	float yScale = 7200.0f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -13,14 +14,14 @@ public class Background : MonoBehaviour {
 		_bgs[0].position += Vector3.down*amtToMove;
 		_bgs[1].position += Vector3.down*amtToMove;
 		
-		if (_bgs[0].position.y <= -500.0f) // bg's size is 500
+		if (_bgs[0].position.y <= -yScale) // bg's size is 500
 		{
-			_bgs[0].position += Vector3.up*1000.0f; // move bg behind the other bg
+			_bgs[0].position += Vector3.up*yScale*2; // move bg behind the other bg
 		}
 		
-		else if (_bgs[1].position.y <= -500.0f)
+		else if (_bgs[1].position.y <= -yScale)
 		{
-			_bgs[1].position += Vector3.up*1000.0f;
+			_bgs[1].position += Vector3.up*yScale*2;
 		}
 	}
 }
