a4bdc36cf61ba63bae09053ea23aaec6583170c0
Davee Nguyen <vibrantdavee@gmail.com>
2013-07-29 16:14:32 -0700

    Copy new game master over

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,6 +3,7 @@
  */
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameMaster : MonoBehaviour {
 	StartMenu settings;
@@ -15,26 +16,33 @@ public class GameMaster : MonoBehaviour {
 	int curLevel;
 	int player1Lives;
 	int player2Lives;
+	public GameObject playerBullet;
+	public static Stack<GameObject> playerBulletStack = new Stack<GameObject>(); // a stack to store all the player bullets
+	public static Stack<GameObject> enemyBulletStack = new Stack<GameObject>(); // a stack to store all the enemy bullets
 	
 	// Use this for initialization
 	void Start () {
-		if( PlayerPrefs.HasKey( "sfxVol" ) )
-			sfxVol = PlayerPrefs.GetInt( "sfxVol" );
-		else
-			sfxVol = 50;
+//		if( PlayerPrefs.HasKey( "sfxVol" ) )
+//			sfxVol = PlayerPrefs.GetInt( "sfxVol" );
+//		else
+//			sfxVol = 50;
+//		
+//		if( PlayerPrefs.HasKey( "bgmVol" ) )
+//			bgmVol = PlayerPrefs.GetInt( "bgmVol" );
+//		else
+//			bgmVol = 50;
+//		
+//		if( PlayerPrefs.HasKey( "difficulty" ) )
+//			difficulty = PlayerPrefs.GetInt( "difficulty" );
+//		else
+//			difficulty = 5;
 		
-		if( PlayerPrefs.HasKey( "bgmVol" ) )
-			bgmVol = PlayerPrefs.GetInt( "bgmVol" );
-		else
-			bgmVol = 50;
-		
-		if( PlayerPrefs.HasKey( "difficulty" ) )
-			difficulty = PlayerPrefs.GetInt( "difficulty" );
-		else
-			difficulty = 5;
-		print( sfxVol );
-		print ( bgmVol );
-		print ( difficulty );
+		for (var i = 0; i < 25; i++)
+		{
+			GameObject newBullet = Instantiate (playerBullet, Vector3.zero, Quaternion.identity) as GameObject; // create a bullet
+			newBullet.gameObject.SetActive(false); // disable it until it's needed
+			playerBulletStack.Push(newBullet); // put it on the stack
+		}
 	}
 	
 	// Update is called once per frame
