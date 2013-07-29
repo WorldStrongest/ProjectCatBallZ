a639dd3beb826b136c453fabf53a83c8dc32507f
Davee Nguyen <vibrantdavee@gmail.com>
2013-07-29 16:33:39 -0700

    Added enemy bullet stack

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -8,45 +8,65 @@ using System.Collections.Generic;
 public class GameMaster : MonoBehaviour {
 	StartMenu settings;
 	Player player1;
-	Player player2;
+	
 	public int difficulty;
 	public int sfxVol;
 	public int bgmVol;
-	int curScore;
-	int curLevel;
-	int player1Lives;
-	int player2Lives;
-	public GameObject playerBullet;
-	public static Stack<GameObject> playerBulletStack = new Stack<GameObject>(); // a stack to store all the player bullets
-	public static Stack<GameObject> enemyBulletStack = new Stack<GameObject>(); // a stack to store all the enemy bullets
+
+	public static int lives;	// Player lives
+	public static int score;	// Player score
+	public static int level;	// Current Level
+	
+	public GameObject playerBullet;	// Player Bullet Prefab
+	public GameObject enemyBullet;	// Enemy Bullet Prefab
+	
+	public int playerBulletPoolSize = 25;
+	public int enemyBulletPoolSize  = 25;
+	
+	public static Stack<GameObject> playerBulletStack = new Stack<GameObject>();	// a stack to store all the player bullets
+	public static Stack<GameObject> enemyBulletStack = new Stack<GameObject>();		// a stack to store all the enemy bullets
 	
 	// Use this for initialization
 	void Start () {
-//		if( PlayerPrefs.HasKey( "sfxVol" ) )
-//			sfxVol = PlayerPrefs.GetInt( "sfxVol" );
-//		else
-//			sfxVol = 50;
-//		
-//		if( PlayerPrefs.HasKey( "bgmVol" ) )
-//			bgmVol = PlayerPrefs.GetInt( "bgmVol" );
-//		else
-//			bgmVol = 50;
-//		
-//		if( PlayerPrefs.HasKey( "difficulty" ) )
-//			difficulty = PlayerPrefs.GetInt( "difficulty" );
-//		else
-//			difficulty = 5;
 		
-		for (var i = 0; i < 25; i++)
-		{
-			GameObject newBullet = Instantiate (playerBullet, Vector3.zero, Quaternion.identity) as GameObject; // create a bullet
-			newBullet.gameObject.SetActive(false); // disable it until it's needed
-			playerBulletStack.Push(newBullet); // put it on the stack
-		}
+		if( PlayerPrefs.HasKey( "sfxVol" ) )
+			sfxVol = PlayerPrefs.GetInt( "sfxVol" );
+		else
+			sfxVol = 50;
+		
+		if( PlayerPrefs.HasKey( "bgmVol" ) )
+			bgmVol = PlayerPrefs.GetInt( "bgmVol" );
+		else
+			bgmVol = 50;
+		
+		if( PlayerPrefs.HasKey( "difficulty" ) )
+			difficulty = PlayerPrefs.GetInt( "difficulty" );
+		else
+			difficulty = 5;
+
+		
+		InitBulletPools();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// Initiates the Bullet Pools for player and enemy
+	void InitBulletPools() {
+		
+		// Player Bullet Pool
+		for (var i = 0; i < playerBulletPoolSize; i++)
+		{
+			GameObject newBullet = Instantiate (playerBullet, Vector3.zero, Quaternion.identity) as GameObject; // create a bullet
+			newBullet.gameObject.SetActive(false);	// disable it until it's needed
+			playerBulletStack.Push(newBullet);		// put it on the stack
+		}
+		
+		// Enemy Bullet Pool
+		for (var i = 0; i < enemyBulletPoolSize; i++)
+		{
+			GameObject newBullet = Instantiate (enemyBullet, Vector3.zero, Quaternion.identity) as GameObject; // create a bullet
+			newBullet.gameObject.SetActive(false);	// disable it until it's needed
+			enemyBulletStack.Push(newBullet);		// put it on the stack
+		}
+		
 	}
 }
 
