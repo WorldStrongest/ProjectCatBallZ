0e3d17f2e7520821e305f0f9efaade2765148e84
Davee Nguyen <vibrantdavee@gmail.com>
2013-07-29 22:08:23 -0700

    Updated Enemy4. Tweaked rigidbody on prefabs

--- Assets/Scripts/Enemy4.cs
+++ Assets/Scripts/Enemy4.cs
@@ -1,7 +1,4 @@
-//Enemy version 4
-//break removed to allow firing 2 shots at the same time
-//Laggy
-
+// Enemy 4 mod
 using UnityEngine;
 using System.Collections;
 
@@ -13,95 +10,65 @@ public class enemyData{
 	public float displacementX;
 	public float displacementY;
 	public int loopCount;
-	public bool shotFired;
 	public bool targetFire;
 	public bool randomAngle;
 };
 
-public class Enemy4 : Enemy {
+public class Enemy4: Enemy {
 	public enemyData[] enemyDATA;
-	public float[] nextBullet;
-	public int totalShots;
-	public int localCounter;
-	int curShot;
 	public bool suicideOnComplete;
-	public bool isBullet;
-	bool firstShot;
-	
+	int curShot; // The index of the current shot
+	int totalShots;	// The total number of shots this enemy has
 
-	
-	// Use this for initialization
-	void Start () {
-		_transform = transform;
+	// float[] nextBullet;
+	// int localCounter
+	// int curShot;
+	// 
+
+	void Start() {
+		_transform = transform;	// cache this transform
 		SetEnemyPath( nodes, easeType, loopType );
-		totalShots = enemyDATA.Length;
-		nextShot = cooldown;
-		nextBullet = new float[totalShots];
-		firstShot = true;
-		Target( target );
-		
-		for( int i = 0; i < totalShots; ++i ){
-			nextBullet[i] = enemyDATA[i].nextCD;
-			enemyDATA[i].shotFired = false;
-		}
-		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-//		_transform.position += Vector3.down*(speed*Time.deltaTime);
-		
-		if( Time.time > nextShot ){
-			if( firstShot ){
-				firstShot = false;
-				nextBullet[0] = Time.time + enemyDATA[0].nextCD;
-				enemyDATA[0].shotFired = false;
-				curShot = 0;
-				for( int i = 1; i < totalShots; ++i ){
-					nextBullet[i] = nextBullet[i-1] + enemyDATA[i].nextCD;
-					enemyDATA[i].shotFired = false;
-				}
-			}
-			
 
-			for( int i = 0; i <totalShots; ++i ){
-				if( !enemyDATA[i].shotFired && Time.time > nextBullet[i] ){
-					if( enemyDATA[i].targetFire )
-						Instantiate( enemyDATA[i].bullet, new Vector3( _transform.position.x + enemyDATA[i].displacementX, _transform.position.y + enemyDATA[i].displacementY, 0 ), Quaternion.LookRotation( Vector3.forward, _target.position - _transform.position ) );
-					else if( enemyDATA[i].randomAngle )
-						Instantiate( enemyDATA[i].bullet, new Vector3( _transform.position.x + enemyDATA[i].displacementX, _transform.position.y + enemyDATA[i].displacementY, 0 ), Quaternion.Euler( 0, 0, Random.Range( -enemyDATA[i].angle, enemyDATA[i].angle ) + 180 ) );
-					else if( enemyDATA[i].angle != 0 )
-						Instantiate( enemyDATA[i].bullet, new Vector3( _transform.position.x + enemyDATA[i].displacementX, _transform.position.y + enemyDATA[i].displacementY, 0 ), Quaternion.Euler( 0, 0, enemyDATA[i].angle + 180 ) );
-					else
-						Instantiate( enemyDATA[i].bullet, new Vector3( _transform.position.x + enemyDATA[i].displacementX, _transform.position.y + enemyDATA[i].displacementY, 0 ), Quaternion.Euler( _transform.rotation.x, _transform.rotation.y, _transform.rotation.z + 180 ) );
-					enemyDATA[i].shotFired = true;
-					
-					if( i == totalShots - 1 ){
-						if( suicideOnComplete ){
-							Destroy( gameObject );
-						}
-						firstShot = true;
-						enemyDATA[totalShots - 1].shotFired = true;
+		curShot = 0;
+		totalShots = enemyDATA.Length;
+		nextShot = Time.time + enemyDATA[0].nextCD;
+	}
+
+	void Update() {
+		if ( Time.time > nextShot ) {
+			do {
+				// get a bullet from the stack
+				// need to figure out which stack to pull bullet from based on enemyDATA[curShot].bullet
+				//GameObject newBullet = GameMaster.enemyBulletStack.Pop();
+				GameObject newBullet = Instantiate ( enemyDATA[curShot].bullet ) as GameObject;
+				
+				// rotation is different in these
+				if ( enemyDATA[curShot].targetFire ) {
+					newBullet.transform.rotation = Quaternion.LookRotation( Vector3.forward, _target.position - _transform.position );
+				} else if ( enemyDATA[curShot].randomAngle ) {
+					newBullet.transform.rotation = Quaternion.Euler( 0, 0, Random.Range( -enemyDATA[curShot].angle, enemyDATA[curShot].angle ) + 180 );
+				} else if ( enemyDATA[curShot].angle != 0 ) {
+					newBullet.transform.rotation = Quaternion.Euler( 0, 0, enemyDATA[curShot].angle + 180 );
+				} else {
+					newBullet.transform.rotation = Quaternion.Euler( _transform.rotation.x, _transform.rotation.y, _transform.rotation.z + 180 ) ;
+				}
+
+				// position and enable it
+				newBullet.transform.position =  _transform.position + new Vector3( enemyDATA[curShot].displacementX, enemyDATA[curShot].displacementY, 0);
+				newBullet.SetActive(true);
+
+				++curShot;
+				if (curShot >= totalShots) {
+					if (suicideOnComplete)
+						Explode();
+					else {
+						curShot = 0;
 						nextShot = Time.time + cooldown;
 					}
-//					break;
+				} else {
+					nextShot = Time.time + enemyDATA[curShot].nextCD;
 				}
-			}
+			} while (enemyDATA[curShot].nextCD == 0);
 		}
 	}
-	
-//	void OnTriggerEnter( Collider collider ){
-//		if( isBullet ){
-//			return;
-//		}
-//		else if( collider.gameObject.tag == "Player" || hitPoints == 0){
-//			Instantiate( onDeathBullet, _transform.position, Quaternion.LookRotation(Vector3.forward, _target.transform.position - _transform.position) ); 
-//			
-//			Destroy( gameObject );
-//			
-//			Debug.Log (collider.transform.position);
-//		}
-//	}
 }
-
